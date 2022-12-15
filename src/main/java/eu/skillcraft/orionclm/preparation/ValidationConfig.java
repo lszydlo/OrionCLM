1aab8a2b67302152359c09ab1d7f84b08e8b7780
lszydlo <szydler@gmail.com>
2022-12-15 11:53:18 +0100

    template method

--- src/main/java/eu/skillcraft/orionclm/preparation/ValidationConfig.java
+++ src/main/java/eu/skillcraft/orionclm/preparation/ValidationConfig.java
@@ -2,4 +2,7 @@ package eu.skillcraft.orionclm.preparation;
 
 public class ValidationConfig {
 
+  public boolean contains(String price) {
+    return false;
+  }
 }
--- src/main/java/eu/skillcraft/orionclm/preparation/ValidatorFactory.java
+++ src/main/java/eu/skillcraft/orionclm/preparation/ValidatorFactory.java
@@ -1,6 +1,7 @@
 package eu.skillcraft.orionclm.preparation;
 
 import eu.skillcraft.orionclm.preparation.PreparationService.Section;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ValidatorFactory {
@@ -12,6 +13,7 @@ public class ValidatorFactory {
   }
 
   public interface ContractValidator {
+
     CValidationErrors validate(List<Section> sections);
   }
 
@@ -25,12 +27,77 @@ public class ValidatorFactory {
   }
 
   interface Checker {
+
     void check(List<Section> sections, ValidationConfig config, CValidationErrors errors);
 
   }
 
 
+  class PriceListChecker implements Checker {
+
+    private final ValidationConfig config;
+
+    public PriceListChecker(ValidationConfig config) {
+      this.config = config;
+    }
+
+    @Override
+    public void check(List<Section> sections, ValidationConfig config, CValidationErrors errors) {
+
+      if (config.contains("PRICE")) {
+        if (isNotValid(sections)) {
+          errors.append("price list missing");
+        }
+      }
+    }
+
+    private boolean isNotValid(List<Section> sections) {
+      ///
+      return true;
+    }
+  }
+
+  class TermsChecker implements Checker {
+
+    @Override
+    public void check(List<Section> sections, ValidationConfig config, CValidationErrors errors) {
+      if (config.contains("TERMS")) {
+        if (isNotValid(sections)) {
+          errors.append("terms are missing");
+        }
+      }
+    }
+
+    private boolean isNotValid(List<Section> sections) {
+      ///
+      return true;
+    }
+  }
+
+  class ContractorChecker implements Checker {
+
+    @Override
+    public void check(List<Section> sections, ValidationConfig config, CValidationErrors errors) {
+      if (config.contains("CONTRACTOR")) {
+        if (isNotValid(sections)) {
+          errors.append("contractor missing");
+        }
+      }
+    }
+
+    private boolean isNotValid(List<Section> sections) {
+      ///
+      return true;
+    }
+  }
+
+
   public static class CValidationErrors {
 
+    List<String> errors = new ArrayList<>();
+
+    public void append(String error) {
+      errors.add(error);
+    }
   }
 }
